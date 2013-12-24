148319ec1c8df019afedd79584faeb6ce05e077d
Nelson Elhage <nelhage@nelhage.com>
2013-12-24 19:30:28 +0200

    Factor config-loading into a separate repo.

--- config.go
+++ config.go
@@ -1,12 +1,10 @@
 package pw
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
-	"io"
+	"github.com/nelhage/go.config"
 	"os"
-	"strings"
 )
 
 type Config struct {
@@ -22,7 +20,7 @@ func init() {
 }
 
 func LoadConfig() *Config {
-	if err := theConfig.loadConfig(); err != nil {
+	if err := config.LoadConfig(flag.CommandLine, "pw"); err != nil {
 		panic(fmt.Sprintf("Loading config: %s", err))
 	}
 	return &theConfig
@@ -31,46 +29,6 @@ func LoadConfig() *Config {
 func (config *Config) recipients() []string {
 	if config.GPGKey != "" {
 		return []string{config.GPGKey}
-	}
-	return nil
-}
-
-func (config *Config) loadConfig() error {
-	path := os.ExpandEnv("${HOME}/.pw")
-	f, err := os.Open(path)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil
-		}
-		return err
-	}
-	defer f.Close()
-	return config.parseConfig(f)
-}
-
-func (config *Config) parseConfig(f io.Reader) error {
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if line == "" || strings.HasPrefix(line, "#") {
-			continue
-		}
-
-		bits := strings.SplitN(line, "=", 2)
-		if len(bits) != 2 {
-			return fmt.Errorf("Illegal config line: `%s'", line)
-		}
-
-		key := strings.TrimSpace(bits[0])
-		value := strings.TrimSpace(bits[1])
-
-		if flag := flag.CommandLine.Lookup(key); flag == nil {
-			return fmt.Errorf("Unknown option `%s'", bits[0])
-		}
-
-		if err := flag.CommandLine.Set(key, value); err != nil {
-			return err
-		}
 	}
 	return nil
 }
