42e6a9eab4aab9cfc6a723f6f70c41054e8dd114
Nelson Elhage <nelhage@nelhage.com>
2014-01-02 14:08:39 +0100

    Add some test cases for flag completion.

--- completion/completion_test.go
+++ completion/completion_test.go
@@ -1,6 +1,7 @@
 package completion
 
 import (
+	"flag"
 	. "launchpad.net/gocheck"
 	"testing"
 )
@@ -30,7 +31,50 @@ func (s *CompletionSuite) TestParseLine(c *C) {
 	for _, tc := range testCases {
 		line := tc.beforePoint + tc.afterPoint
 		point := len(tc.beforePoint)
-		st := parseLineForCompletion(line, point)
-		c.Check(st.Words, DeepEquals, tc.words)
+		cl := parseLineForCompletion(line, point)
+		c.Check([]string(cl), DeepEquals, tc.words)
+	}
+}
+
+type FlagCompletionSuite struct {
+	flags flag.FlagSet
+}
+
+var _ = Suite(&FlagCompletionSuite{})
+
+func (s *FlagCompletionSuite) SetUpSuite(c *C) {
+	s.flags.Bool("bool", false, "bool flag")
+	s.flags.Int("int", 0, "int flag")
+	s.flags.String("str", "", "string flag")
+	s.flags.String("str1", "", "string flag 1")
+}
+
+func (s *FlagCompletionSuite) TestCompleteFlags(c *C) {
+	testCases := []struct {
+		commandLine []string
+		completions []string
+		skip        int
+	}{
+		{[]string{"-"}, []string{"-bool", "-int", "-str", "-str1"}, 0},
+		{[]string{""}, nil, 0},
+		{[]string{"-bool", ""}, nil, 1},
+		{[]string{"-int", "7", ""}, nil, 2},
+		{[]string{"-bool", "-str", ""}, []string{}, 0},
+		{[]string{"-bool", "-str"}, []string{"-str", "-str1"}, 0},
+		{[]string{"-str", "hello", "--int"}, []string{"-int"}, 0},
+		{[]string{"-str", "hello", "--int", "42", "", "world"}, nil, 4},
+		{[]string{"-str", "hello", "--int", "42", "--", "-str"}, nil, 5},
+		{[]string{"-wtf", "-value", ""}, nil, 2},
+	}
+	for _, tc := range testCases {
+		var cl CommandLine = append(CommandLine{"cmd"}, tc.commandLine...)
+		completions, rest := completeFlags(cl, &s.flags)
+		if tc.completions != nil {
+			c.Check(completions, DeepEquals, tc.completions)
+			c.Check(rest, IsNil)
+		} else {
+			c.Check(completions, IsNil)
+			c.Check(rest, DeepEquals, cl[tc.skip+1:])
+		}
 	}
 }
